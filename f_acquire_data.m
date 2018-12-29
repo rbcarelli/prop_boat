d394da0f463cb81e80186633f40db35e4f713e98
rbcarelli <rbcarelli@gmail.com>
2018-12-28 22:30:57 -0500

    fix bugs

--- f_acquire_data.m
+++ f_acquire_data.m
@@ -1,7 +1,7 @@
 
 function f_acquire_data()
     f_reset_plots()
-  
+      
     global enable_acquire raw_thrust raw_torque raw_rpm program_running...
     time mydaq packet_size sample_rate
     
@@ -17,10 +17,22 @@ function f_acquire_data()
     while enable_acquire && program_running
         fprintf('While loop: Scans acquired = %d\n', mydaq.ScansAcquired)
         f_plot_data([raw_thrust raw_torque],time,[2 2 1])
-        %f_plot_data(raw_rpm,sample_rate,time,[2 2 2])        
-         pause(.1)
+        %rpm = tachorpm(raw_rpm, sample_rate);
+        f_plot_data(raw_rpm,time,[2 2 2])        
+        pause(.1)
         
     end   
+    
+    text = strcat('Input advance ratio');
+    prompt = {text};
+    title = 'Advance Ratio';
+    dims = [1 30];
+    definput = {''};
+    advance_ratio = string(inputdlg(prompt,title,dims,definput));
+    
+    filename = strcat('C:\Users\rbcar\Documents\MIT\~Thesis\Data\',advance_ratio)
+    data = [time raw_thrust raw_torque raw_rpm];
+    csvwrite(filename, data)
     
     delete(lh)
     
